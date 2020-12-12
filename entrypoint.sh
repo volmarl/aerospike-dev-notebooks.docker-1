eeea329c919a1084d0f7d917b02951c1fea940c3
Lucien Volmar <lucien@aerospike.com>
2020-12-12 11:38:23 -0800

    startup logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,24 +29,10 @@ if [ -f /etc/aerospike/aerospike.template.conf ]; then
         envsubst < /etc/aerospike/aerospike.template.conf > /etc/aerospike/aerospike.conf
 fi
 
-NETLINK=${NETLINK:-eth0}
-
-# we will wait a bit for the network link to be up.
-NETLINK_UP=0
-NETLINK_COUNT=0
-echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate)"
-while [ $NETLINK_UP -eq 0 ] && [ $NETLINK_COUNT -lt 20 ]; do
-    if grep -q "up" /sys/class/net/${NETLINK}/operstate; then
-                NETLINK_UP=1
-        else
-                sleep 0.1
-                let NETLINK_COUNT=NETLINK_COUNT+1
-        fi
-done
-echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate) in ${NETLINK_COUNT}"
-
-#service aerospike restart
-/etc/init.d/aerospike start 2>&1 >> /var/log/aerospike/start-up.log
+echo "starting aerospike"
+service aerospike restart
+echo $?
+#/etc/init.d/aerospike start 2>&1 >> /var/log/aerospike/start-up.log
 
 #####
 # Jupiter stuff
